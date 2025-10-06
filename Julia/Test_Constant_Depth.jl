8a1ae90961ff2ff8cbc34b1745fecdd4e92cacc6
Fidel Luis Como <99187459+FidelComo@users.noreply.github.com>
2025-10-06 15:27:14 -0300

    Depth One

---
+++ Julia/Test_Constant_Depth.jl
@@ -0,0 +1,44 @@
+using MATLAB
+using JSON
+
+mat"addpath('./MatlabCode/MATLAB')"
+mat"addpath('../')"
+T = 20
+M = 10.0
+AddP = 5
+path = ""
+
+Times = zeros(5,2)
+Costs = zeros(5,2)
+CV = zeros(4,2)
+sigmaN = 0.2
+phinp,Nb,NS,gamma,Inflow_Noises,sigmaNoises,Probabilities,Thermal_Costs,Mus,sigmaInflows,phi,Recent_History_Inflows,Tendancy_Inflows,TabP,TabS,vminT,vmaxT,vminH,vmaxH,xmax,x0,lambda,evap,Demand = mxcall(:init_data_hydro_sddp,24,T,M,2,path,sigmaN,AddP)
+NS = Int(NS)
+lambda = 0 * ones(T,NS)
+talpha = 1.96
+N = 100
+tol = 0.05
+Itermax = 2000
+S=1
+
+#CDDR depth=1
+path = ""
+depth=1                                           
+subi_a,subj_a,valij_a,betas,cost,ds,ps,qs,probabilities = mxcall(:init_data_hydro_cddr,9,depth,T,M,path,AddP,Inflow_Noises)
+#For initialization with b_t of type depth>=1
+#[subi_a,subj_a,valij_a,betas,cost,ds,ps,qs,probabilities]=init_data_cddr(2,T,M,1,path,sigmasN,AddP,Inflow_Noises,2);
+
+subi_a = JSON.parse(subi_a)
+subj_a = JSON.parse(subj_a)
+valij_a = JSON.parse(valij_a)
+
+ps = Int.(ps)
+qs = Int.(qs)
+ds = Int.(ds)
+
+sol,opt_value,out,time1,time2,nvars,counter = solve_constant_depth_decision_rules_depth_one(subi_a,subj_a,valij_a,betas,cost,probabilities,ds,T,ps,qs)
+Costs[2,1] = opt_value
+CV[1,1] = nvars
+CV[1,2] = counter
+Times[2,1] = time1
+Times[2,2] = time2
---
+++ Julia/solve_constant_depth_decision_rules_depth_one.jl
@@ -0,0 +1,103 @@
+using JuMP
+using HiGHS
+
+###############################################################################
+#Inputs
+###############################################################################
+#Matrix A^{t tau} is given in sparse format by 
+#subi_a[t,tau], subj_a[t,tau], valij_a[t,tau]. 
+#beta[t,s,ell,i] is beta^{t i}_{s scenario(ell)} of the paper where 
+#scenario(ell) is the scenario numbered ell.
+#cost[t] is the cost vector for stage t
+#probabilities[t-1,k] is the probability of kth realization for stage t=2,..,T.
+#ds[t] is the number of realizations for stage t
+#T is the number of stages
+#A^{t tau} has size ps[t]*qs[t] 
+###############################################################################
+#Outputs
+###############################################################################
+#sol: optimal solution 
+#opt_value: optimal value
+#out=0: optimal solution found; out=1: infeasible; out=2: primal infinite
+#opt value
+#time1: time to define the optimization problem
+#time2: time to solve the optimization problem
+#nvars: number of variables of the problem
+#nconst: number of constraints of the problem
+###############################################################################
+
+function solve_constant_depth_decision_rules_depth_one(subi_a,subj_a,valij_a,betas,cost,probabilities,ds,T,ps,qs)
+    time1 = @elapsed begin
+        
+        CDDR1 = Model(HiGHS.Optimizer)
+
+        #Variables
+        @variable(CDDR1, v[t = 1:T, i = 1:ps[t], s = 1:t, k = 1:ds[s]])
+        @constraint(CDDR1, v[1,:,:,:] .<= 0)
+        @variable(CDDR1, u[t = 1:T, i = 1:qs[t], s = 1:t, k = 1:ds[s]])
+        @variable(CDDR1, z[t = 2:T, i = 1:ps[t], s = 2:t])
+        
+        
+        #Constraints
+        nconst = 0
+        for t=1:T, s=1:t, i=1:ps[t], eta=1:ds[s]
+            var = []
+            val = []
+            for tau=s:t, k in findall(subi_a[t][tau] .== i)
+                append!(var, u[tau,subj_a[t][tau][k],s,eta])
+                append!(val, valij_a[t][tau][k])
+            end
+            @constraint(CDDR1, v[t,i,s,eta] - transpose(val)*var == -betas[t][s][eta,i])
+            nconst += 1
+        end
+
+        for t=2:T, i=1:ps[t]
+            s = 2:t
+            @constraint(CDDR1, v[t,i,1,1] + sum(z[t,i,s]) <= 0)
+            nconst += 1
+        end
+
+        for t=2:T, i=1:ps[t], s=2:t, k=1:ds[s]
+            @constraint(CDDR1, z[t,i,s] - v[t,i,s,k] >= 0)
+            nconst += 1
+        end
+
+        #Objective
+        c=zero(u)
+        for t=1:T, s=1:t, i=1:qs[t], k=1:ds[s]
+            if s >= 2
+                c[t,i,s,k] = cost[t][i]*probabilities[s-1,k]
+            else
+                c[t,i,s,k] = cost[t][i]
+            end
+        end
+
+        @objective(CDDR1, Min, sum(c.*u))
+    end
+
+    time2 = @elapsed begin
+        #Solution
+        oldstd = stdout
+        redirect_stdout(devnull)
+        optimize!(CDDR1)
+        redirect_stdout(oldstd)
+
+        sol = [JuMP.value.(u), JuMP.value.(v), JuMP.value.(z)]
+
+        if JuMP.INFEASIBLE == 1
+            print("Infeasible problem")
+            out=1
+            opt_value=Inf
+        elseif JuMP.DUAL_INFEASIBLE == 1
+            print("Primal infinite optimal value")
+            out=2
+            opt_value=-Inf
+        else
+            out=0
+            opt_value = objective_value(CDDR1)
+        end
+        nvar = num_variables(CDDR1)
+    end
+
+    return sol, opt_value, out, time1, time2, nvar, nconst
+end
